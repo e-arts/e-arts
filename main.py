3293a35adf1610694e98b19753c66c6715d46e12
Lettner Daniel <48688085+daniellettner@users.noreply.github.com>
2023-01-07 15:16:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello World!"
+    return 'Hello World!'
 
 
-app.run(host="0.0.0.0", port=81)
+if __name__ == '__main__':
+    port = 5001
+    app.run(host='0.0.0.0', port=port)
